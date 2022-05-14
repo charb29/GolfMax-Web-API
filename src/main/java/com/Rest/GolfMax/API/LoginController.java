8f943da393f9046b662a6db157bf58ea14b128a3
charb29 <ocharb@mac.com>
2022-05-13 22:32:53 -0700

    fix login request

--- src/main/java/com/Rest/GolfMax/API/LoginController.java
+++ src/main/java/com/Rest/GolfMax/API/LoginController.java
@@ -6,7 +6,6 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
