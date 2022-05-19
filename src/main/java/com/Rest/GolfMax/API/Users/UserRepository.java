cdb34b9fae9dbe43f3e5b34a63170148128e2ff4
charb29 <ocharb@mac.com>
2022-05-19 13:42:29 -0700

    updated database

--- src/main/java/com/Rest/GolfMax/API/Users/UserRepository.java
+++ src/main/java/com/Rest/GolfMax/API/Users/UserRepository.java
@@ -4,9 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
