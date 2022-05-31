5521a0b0fa89430207c1ad1c638a481e986721b5
charb29 <ocharb@mac.com>
2022-05-30 22:33:48 -0700

    Deleted

--- src/main/java/com/Rest/GolfMax/API/Scores/ScoreRepository.java
+++ src/main/java/com/Rest/GolfMax/API/Scores/ScoreRepository.java
@@ -1,7 +1,5 @@
 package com.Rest.GolfMax.API.Scores;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
