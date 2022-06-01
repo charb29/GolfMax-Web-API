fba9eb3dce3fbed18cdcb6269b12f027a4b07829
charb29 <ocharb@mac.com>
2022-06-01 15:46:24 -0700

    Course Mappings

--- src/main/java/com/Rest/GolfMax/API/Controllers/CourseController.java
+++ src/main/java/com/Rest/GolfMax/API/Controllers/CourseController.java
@@ -1,49 +1,37 @@
 package com.Rest.GolfMax.API.Controllers;
-
 import com.Rest.GolfMax.API.Models.Course;
-import com.Rest.GolfMax.API.Models.Score;
 import com.Rest.GolfMax.API.Repositories.CourseRepository;
-import com.Rest.GolfMax.API.Services.CourseService;
+import java.util.List;
+import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.NoSuchElementException;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.Rest.GolfMax.API.Services.CourseService;
 
 @RestController
 @RequestMapping("/courses")
 public class CourseController {
+    
+    @Autowired
+    CourseRepository courseRepository;
 
     @Autowired
     CourseService courseService;
 
-    @Autowired
-    CourseRepository courseRepository;
-
     @GetMapping("")
-    public List<Course> getAllCourses() {
+    public List<Course> listAllCourses() {
         return courseService.listAllCourses();
     }
 
-    @GetMapping("/course-names")
-    public List<String> getAllCourseNames() {
-        return courseService.listAllCourseNames();
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Course> getCourseById(@PathVariable Long id) {
-        try {
-            Course course = courseService.getCourseByCourseId(id);
-            return new ResponseEntity<Course> (course, HttpStatus.OK);
-        } catch (NoSuchElementException e) {
-            return new ResponseEntity<Course> (HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @PostMapping("/new")
-    public ResponseEntity<Course> addCourse(@RequestBody Course course) {
+    @PostMapping("/new_course")
+    public ResponseEntity<Course> addNewCourse(@RequestBody Course course) {
         if (courseRepository.existsByCourseName(course.getCourseName())) {
             return new ResponseEntity<>(course, HttpStatus.BAD_REQUEST);
         }
@@ -51,8 +39,18 @@ public class CourseController {
         return new ResponseEntity<Course>(course, HttpStatus.CREATED);
     }
 
+    @GetMapping("{id}")
+    public ResponseEntity<Course> getCourseById(@PathVariable Long id) {
+        try {
+            Course course = courseService.getCourseById(id);
+            return new ResponseEntity<Course>(course, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<Course>(HttpStatus.NOT_FOUND);
+        }
+    }
+
     @DeleteMapping("/{id}")
-    public void deleteCourse(@PathVariable Long id) {
+    public void deleteUser(@PathVariable Long id) {
         courseService.deleteCourse(id);
     }
 }
