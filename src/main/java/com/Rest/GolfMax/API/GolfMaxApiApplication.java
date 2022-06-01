c9f522785218956d1f2c9e108d058b4ee3cb9437
charb29 <ocharb@mac.com>
2022-06-01 15:46:10 -0700

    Deleted a space

--- src/main/java/com/Rest/GolfMax/API/GolfMaxApiApplication.java
+++ src/main/java/com/Rest/GolfMax/API/GolfMaxApiApplication.java
@@ -2,13 +2,10 @@ package com.Rest.GolfMax.API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GolfMaxApiApplication {
 
-
-	
 	public static void main(String[] args) {
 		SpringApplication.run(GolfMaxApiApplication.class, args);
 	}
