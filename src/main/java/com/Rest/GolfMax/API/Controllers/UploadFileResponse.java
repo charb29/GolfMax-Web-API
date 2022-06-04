6d9f3ca4cad2595efffda743355de457e9c182d0
charb29 <ocharb@mac.com>
2022-06-03 22:04:18 -0700

    adding file id to the response

--- src/main/java/com/Rest/GolfMax/API/Controllers/UploadFileResponse.java
+++ src/main/java/com/Rest/GolfMax/API/Controllers/UploadFileResponse.java
@@ -1,16 +1,19 @@
 package com.Rest.GolfMax.API.Controllers;
 
 public class UploadFileResponse {
+
     private String fileName;
     private String fileDownloadUri;
     private String fileType;
     private long size;
+    private long id;
 
-    public UploadFileResponse(String fileName, String fileDownloadUri, String fileType, long size) {
+    public UploadFileResponse(String fileName, String fileDownloadUri, String fileType, long size, long id) {
         this.fileName = fileName;
         this.fileDownloadUri = fileDownloadUri;
         this.fileType = fileType;
         this.size = size;
+        this.id = id;
     }
 
     public String getFileName() {
@@ -43,5 +46,13 @@ public class UploadFileResponse {
 
     public void setSize(long size) {
         this.size = size;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 }
