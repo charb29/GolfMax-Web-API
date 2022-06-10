e2288f740f448638be9624ec7f57e9ac90a45f24
charb29 <ocharb@mac.com>
2022-06-09 22:59:05 -0700

    deleted

--- src/main/java/com/Rest/GolfMax/API/Repositories/CourseImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Rest.GolfMax.API.Repositories;
-
-import com.Rest.GolfMax.API.Models.CourseImageFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseImageRepository extends JpaRepository<CourseImageFile, Long>{
-}
