98d5de8608fa27566374e26b3fc75f3f76671306
charb29 <ocharb@mac.com>
2022-06-26 17:16:55 -0700

    LoginController TEST

---
+++ src/test/java/com/Rest/GolfMax/API/LoginControllerTest.java
@@ -0,0 +1,47 @@
+package com.Rest.GolfMax.API;
+import com.Rest.GolfMax.API.Controllers.LoginController;
+import com.Rest.GolfMax.API.Models.User;
+import com.Rest.GolfMax.API.Services.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+@WebMvcTest(LoginController.class)
+public class LoginControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @MockBean
+    UserService userService;
+
+    User USER_1 = new User(1, "Olivier", "password", "olivier@gmail.com");
+
+    @Test
+    public void login() throws Exception {
+        Mockito.when(userService.getStoredUserData(USER_1.getUsername(), USER_1.getPassword())).thenReturn(USER_1);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(USER_1));
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(jsonPath("$.username", is("Olivier")));
+    }
+}
