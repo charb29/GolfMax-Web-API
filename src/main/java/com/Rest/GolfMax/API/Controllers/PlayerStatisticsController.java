ca41446173cc397f111073612e5d1bf9b16b881d
charb29 <ocharb@mac.com>
2022-06-29 14:01:51 -0700

    Changed method name

--- src/main/java/com/Rest/GolfMax/API/Controllers/PlayerStatisticsController.java
+++ src/main/java/com/Rest/GolfMax/API/Controllers/PlayerStatisticsController.java
@@ -3,10 +3,7 @@ package com.Rest.GolfMax.API.Controllers;
 import com.Rest.GolfMax.API.Models.PlayerStatistics;
 import com.Rest.GolfMax.API.Services.PlayerStatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -17,7 +14,7 @@ public class PlayerStatisticsController {
     PlayerStatisticsService playerStatisticsService;
 
     @GetMapping("/user/{id}")
-    public PlayerStatistics getUserStats(@PathVariable long id) {
-        return playerStatisticsService.getUserStats(id);
+    public PlayerStatistics getStatsByUserId(@PathVariable long id) {
+        return playerStatisticsService.getStatsByUserId(id);
     }
 }
