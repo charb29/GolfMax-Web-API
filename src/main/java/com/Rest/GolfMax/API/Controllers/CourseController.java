a9221b151dc59ed0aca01f369318e2f228c5f496
charb29 <ocharb@mac.com>
2022-07-18 19:17:18 -0700

    Removed unused imports

--- src/main/java/com/Rest/GolfMax/API/Controllers/CourseController.java
+++ src/main/java/com/Rest/GolfMax/API/Controllers/CourseController.java
@@ -1,19 +1,14 @@
 package com.Rest.GolfMax.API.Controllers;
+
 import com.Rest.GolfMax.API.Models.Course;
-import com.Rest.GolfMax.API.Repositories.CourseRepository;
-import java.util.List;
-import java.util.NoSuchElementException;
+import com.Rest.GolfMax.API.Services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.Rest.GolfMax.API.Services.CourseService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/courses")
