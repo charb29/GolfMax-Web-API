94740499d1d8805c6f5d064c061d1ec8ceaad2b1
Olivier Charbonneau <ocharb@mac.com>
2022-08-10 12:43:44 -0700

    Autowired CourseRepository

--- src/main/java/com/Rest/GolfMax/API/Controllers/CourseController.java
+++ src/main/java/com/Rest/GolfMax/API/Controllers/CourseController.java
@@ -1,7 +1,9 @@
 package com.Rest.GolfMax.API.Controllers;
 
 import com.Rest.GolfMax.API.Models.Course;
+import com.Rest.GolfMax.API.Repositories.CourseRepository;
 import com.Rest.GolfMax.API.Services.CourseService;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,27 +19,30 @@ public class CourseController {
     @Autowired
     CourseService courseService;
 
+    @Autowired
+    CourseRepository courseRepository;
+
     @GetMapping("")
     public List<Course> listAllCourses() {
         return courseService.listAllCourses();
     }
 
     @PostMapping("/new_course")
-    public ResponseEntity<Course> addNewCourse(@RequestBody Course course) {
-        if (courseService.existsByCourseName(course.getCourseName())) {
+    public ResponseEntity<Course> addNewCourse(@RequestBody @NotNull Course course) {
+        if (courseRepository.existsByCourseName(course.getCourseName()))
             return new ResponseEntity<>(course, HttpStatus.BAD_REQUEST);
-        }
-        courseService.saveCourse(course);
-        return new ResponseEntity<Course>(course, HttpStatus.CREATED);
+        else
+            courseRepository.save(course);
+        return new ResponseEntity<>(course, HttpStatus.CREATED);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<Course> getCourseById(@PathVariable Long id) {
         try {
             Course course = courseService.getCourseById(id);
-            return new ResponseEntity<Course>(course, HttpStatus.OK);
+            return new ResponseEntity<>(course, HttpStatus.OK);
         } catch (NoSuchElementException e) {
-            return new ResponseEntity<Course>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
