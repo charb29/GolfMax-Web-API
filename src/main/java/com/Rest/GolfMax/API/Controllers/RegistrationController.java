041be8275bdcad8b6bb618ea553934873f61be19
Olivier Charbonneau <ocharb@mac.com>
2022-08-14 19:02:56 -0700

    Moved from User Package to Controllers Package

--- src/main/java/com/Rest/GolfMax/API/Controllers/RegistrationController.java
+++ src/main/java/com/Rest/GolfMax/API/Controllers/RegistrationController.java
@@ -1,7 +1,7 @@
-package com.Rest.GolfMax.API.User;
+package com.Rest.GolfMax.API.Controllers;
 
-import com.Rest.GolfMax.API.User.User;
-import com.Rest.GolfMax.API.User.UserRepository;
+import com.Rest.GolfMax.API.Models.User;
+import com.Rest.GolfMax.API.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,15 +10,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/users")
 public class RegistrationController {
-
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
 
     @PostMapping("/account")
     public ResponseEntity<User> registerUser(@RequestBody User user) {
-        if (userRepository.existsByUsername(user.getUsername()) || userRepository.existsByEmail(user.getEmail()))
+        if (userService.userExists(user.getUsername(), user.getEmail()))
             return new ResponseEntity<>(user, HttpStatus.BAD_REQUEST);
-        userRepository.save(user);
-        return new ResponseEntity<User>(user, HttpStatus.CREATED);
+        userService.saveUser(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 }
