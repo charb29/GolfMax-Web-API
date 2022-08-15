afd7c383a0e4381bd52db2bf41c5816c2779e32b
Olivier Charbonneau <ocharb@mac.com>
2022-08-15 13:57:51 -0700

    renamed courseRepository to COURSE_REPOSITORY

--- src/main/java/com/Rest/GolfMax/API/Services/CourseService.java
+++ src/main/java/com/Rest/GolfMax/API/Services/CourseService.java
@@ -1,41 +1,41 @@
 package com.Rest.GolfMax.API.Services;
-import java.util.List;
 
+import com.Rest.GolfMax.API.Models.Course;
 import com.Rest.GolfMax.API.Repositories.CourseRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import com.Rest.GolfMax.API.Models.Course;
 import org.springframework.web.context.annotation.RequestScope;
+
+import java.util.List;
 
 
 @Service
 @Transactional
 @RequestScope
 public class CourseService {
-    @Autowired
-    private CourseRepository courseRepository;
+    private final CourseRepository COURSE_REPOSITORY;
+
+    public CourseService(CourseRepository COURSE_REPOSITORY) {
+        this.COURSE_REPOSITORY = COURSE_REPOSITORY;
+    }
 
     public List<Course> listAllCourses() {
-        return courseRepository.findAll();
+        return COURSE_REPOSITORY.findAll();
     }
 
     public void addNewCourse(Course course) {
-        courseRepository.save(course);
+        COURSE_REPOSITORY.save(course);
     }
 
     public Course getCourseById(Long id) {
-        return courseRepository.findById(id).get();
+        return COURSE_REPOSITORY.findById(id).get();
     }
     
     public void deleteCourse(Long id) {
-        courseRepository.deleteById(id);
+        COURSE_REPOSITORY.deleteById(id);
     }
 
     public boolean existsByCourseName(String courseName) {
-        if (courseRepository.existsByCourseName(courseName))
-            return false;
-        else
-            return true;
+        return !COURSE_REPOSITORY.existsByCourseName(courseName);
     }
 }
