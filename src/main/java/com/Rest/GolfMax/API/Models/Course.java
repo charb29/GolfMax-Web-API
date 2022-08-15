86fc64fc19af338fae81b14a72b826b0fe167e6d
Olivier Charbonneau <ocharb@mac.com>
2022-08-14 22:05:11 -0700

    Added Tee boxes to parameterized constructor

--- src/main/java/com/Rest/GolfMax/API/Models/Course.java
+++ src/main/java/com/Rest/GolfMax/API/Models/Course.java
@@ -12,29 +12,24 @@ public class Course {
     private long id;
     @OneToMany(mappedBy = "course")
     private List<Score> scores;
-    @OneToMany(mappedBy = "course")
-    private List<ChampionshipTees> championshipTees;
-    @OneToMany(mappedBy = "course")
-    private List<ChampionshipTeesHoleYardages> championshipTeesHoleYardages;
-    @OneToMany(mappedBy = "course")
-    private List<MenTees> menTees;
-    @OneToMany
-    private List<MenTeesHoleYardages> menTeesHoleYardages;
-    @OneToMany(mappedBy = "course")
-    private List<WomenTees> womenTees;
-    @OneToMany(mappedBy = "course")
-    private List<WomenTeesHoleYardages> womenTeesHoleYardages;
+    @OneToOne
+    private ChampionshipTees championshipTees;
+    @OneToOne
+    private MenTees menTees;
+    @OneToOne
+    private WomenTees womenTees;
     @Column(name = "courseName", nullable = false)
     private String courseName;
-    @Column(name = "overallPar", nullable = false)
-    private int overallPar;
 
     public Course() {}
 
-    public Course(long id, String courseName, int overallPar) {
+    public Course(long id, ChampionshipTees championshipTees, MenTees menTees,
+                  WomenTees womenTees, String courseName) {
         this.id = id;
+        this.championshipTees = championshipTees;
+        this.menTees = menTees;
+        this.womenTees = womenTees;
         this.courseName = courseName;
-        this.overallPar = overallPar;
     }
 
     public long getId() {
@@ -45,60 +40,28 @@ public class Course {
         this.id = id;
     }
 
-    public List<Score> getScores() {
-        return scores;
-    }
-
-    public void setScores(List<Score> scores) {
-        this.scores = scores;
-    }
-
-    public List<ChampionshipTees> getChampionshipTees() {
+    public ChampionshipTees getChampionshipTees() {
         return championshipTees;
     }
 
-    public void setChampionshipTees(List<ChampionshipTees> championshipTees) {
+    public void setChampionshipTees(ChampionshipTees championshipTees) {
         this.championshipTees = championshipTees;
     }
 
-    public List<ChampionshipTeesHoleYardages> getChampionshipTeesHoleYardages() {
-        return championshipTeesHoleYardages;
-    }
-
-    public void setChampionshipTeesHoleYardages(List<ChampionshipTeesHoleYardages> championshipTeesHoleYardages) {
-        this.championshipTeesHoleYardages = championshipTeesHoleYardages;
-    }
-
-    public List<MenTees> getMenTees() {
+    public MenTees getMenTees() {
         return menTees;
     }
 
-    public void setMenTees(List<MenTees> menTees) {
+    public void setMenTees(MenTees menTees) {
         this.menTees = menTees;
     }
 
-    public List<MenTeesHoleYardages> getMenTeesHoleYardages() {
-        return menTeesHoleYardages;
-    }
-
-    public void setMenTeesHoleYardages(List<MenTeesHoleYardages> menTeesHoleYardages) {
-        this.menTeesHoleYardages = menTeesHoleYardages;
-    }
-
-    public List<WomenTees> getWomenTees() {
+    public WomenTees getWomenTees() {
         return womenTees;
     }
 
-    public void setWomenTees(List<WomenTees> womenTees) {
+    public void setWomenTees(WomenTees womenTees) {
         this.womenTees = womenTees;
-    }
-
-    public List<WomenTeesHoleYardages> getWomenTeesHoleYardages() {
-        return womenTeesHoleYardages;
-    }
-
-    public void setWomenTeesHoleYardages(List<WomenTeesHoleYardages> womenTeesHoleYardages) {
-        this.womenTeesHoleYardages = womenTeesHoleYardages;
     }
 
     public String getCourseName() {
@@ -107,13 +70,5 @@ public class Course {
 
     public void setCourseName(String courseName) {
         this.courseName = courseName;
-    }
-
-    public int getOverallPar() {
-        return overallPar;
-    }
-
-    public void setOverallPar(int overallPar) {
-        this.overallPar = overallPar;
     }
 }
