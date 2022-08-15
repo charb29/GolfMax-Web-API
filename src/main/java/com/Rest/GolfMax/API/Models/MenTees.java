b61ecc3a3dc00385b19e11515a5b411b151f4a1d
Olivier Charbonneau <ocharb@mac.com>
2022-08-14 22:04:13 -0700

    Added hole yardages for Tee boxes

--- src/main/java/com/Rest/GolfMax/API/Models/MenTees.java
+++ src/main/java/com/Rest/GolfMax/API/Models/MenTees.java
@@ -1,10 +1,7 @@
 package com.Rest.GolfMax.API.Models;
 
-import org.jetbrains.annotations.NotNull;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "mensTees")
@@ -13,16 +10,12 @@ public class MenTees {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private long id;
-    @OneToOne(mappedBy = "menTees")
-    private MenTeesHoleYardages mensTeesHolesYardages;
-    @ManyToOne
+    @OneToOne(mappedBy = "mensTees")
     private Course course;
     @Column(name = "courseRating", nullable = false)
     private double courseRating;
     @Column(name = "slopeRating", nullable = false)
     private double slopeRating;
-    @Column(name = "totalYards", nullable = false)
-    private long totalYards;
     @Column(name = "hole1", nullable = false)
     private int hole1;
     @Column(name = "hole2", nullable = false)
@@ -59,19 +52,62 @@ public class MenTees {
     private int hole17;
     @Column(name = "hole18")
     private int hole18;
+    @Column(name = "hole1Yards", nullable = false)
+    private int hole1Yards;
+    @Column(name = "hole2Yards", nullable = false)
+    private int hole2Yards;
+    @Column(name = "hole3Yards", nullable = false)
+    private int hole3Yards;
+    @Column(name = "hole4Yards", nullable = false)
+    private int hole4Yards;
+    @Column(name = "hole5Yards", nullable = false)
+    private int hole5Yards;
+    @Column(name = "hole6Yards", nullable = false)
+    private int hole6Yards;
+    @Column(name = "hole7Yards", nullable = false)
+    private int hole7Yards;
+    @Column(name = "hole8Yards", nullable = false)
+    private int hole8Yards;
+    @Column(name = "hole9Yards", nullable = false)
+    private int hole9Yards;
+    @Column(name = "hole10Yards")
+    private int hole10Yards;
+    @Column(name = "hole11Yards")
+    private int hole11Yards;
+    @Column(name = "hole12Yards")
+    private int hole12Yards;
+    @Column(name = "hole13Yards")
+    private int hole13Yards;
+    @Column(name = "hole14Yards")
+    private int hole14Yards;
+    @Column(name = "hole15Yards")
+    private int hole15Yards;
+    @Column(name = "hole16Yards")
+    private int hole16Yards;
+    @Column(name = "hole17Yards")
+    private int hole17Yards;
+    @Column(name = "hole18Yards")
+    private int hole18Yards;
+    @Column(name = "totalFront9Yards", nullable = false)
+    private long totalFront9Yards;
+    @Column(name = "totalBack9Yards", nullable = false)
+    private long totalBack9Yards;
+    @Column(name = "overallPar", nullable = false)
+    private int overallPar;
 
     public MenTees() {}
 
-    public MenTees(long id, MenTeesHoleYardages mensTeesHolesYardages, Course course, double courseRating,
-                   double slopeRating, long totalYards, int hole1, int hole2, int hole3, int hole4, int hole5,
-                   int hole6, int hole7, int hole8, int hole9, int hole10, int hole11, int hole12, int hole13,
-                   int hole14, int hole15, int hole16, int hole17, int hole18) {
+    public MenTees(long id, double courseRating, double slopeRating, int hole1, int hole2, int hole3,
+                   int hole4, int hole5, int hole6, int hole7, int hole8, int hole9, int hole10, int hole11, int hole12,
+                   int hole13, int hole14, int hole15, int hole16, int hole17, int hole18, int hole1Yards,
+                   int hole2Yards, int hole3Yards, int hole4Yards, int hole5Yards, int hole6Yards, int hole7Yards,
+                   int hole8Yards, int hole9Yards, int hole10Yards, int hole11Yards, int hole12Yards, int hole13Yards,
+                   int hole14Yards, int hole15Yards, int hole16Yards, int hole17Yards, int hole18Yards,
+                   long totalFront9Yards, long totalBack9Yards, int overallPar) {
         this.id = id;
-        this.mensTeesHolesYardages = mensTeesHolesYardages;
         this.course = course;
         this.courseRating = courseRating;
         this.slopeRating = slopeRating;
-        this.totalYards = totalYards;
         this.hole1 = hole1;
         this.hole2 = hole2;
         this.hole3 = hole3;
@@ -90,6 +126,27 @@ public class MenTees {
         this.hole16 = hole16;
         this.hole17 = hole17;
         this.hole18 = hole18;
+        this.hole1Yards = hole1Yards;
+        this.hole2Yards = hole2Yards;
+        this.hole3Yards = hole3Yards;
+        this.hole4Yards = hole4Yards;
+        this.hole5Yards = hole5Yards;
+        this.hole6Yards = hole6Yards;
+        this.hole7Yards = hole7Yards;
+        this.hole8Yards = hole8Yards;
+        this.hole9Yards = hole9Yards;
+        this.hole10Yards = hole10Yards;
+        this.hole11Yards = hole11Yards;
+        this.hole12Yards = hole12Yards;
+        this.hole13Yards = hole13Yards;
+        this.hole14Yards = hole14Yards;
+        this.hole15Yards = hole15Yards;
+        this.hole16Yards = hole16Yards;
+        this.hole17Yards = hole17Yards;
+        this.hole18Yards = hole18Yards;
+        this.totalFront9Yards = totalFront9Yards;
+        this.totalBack9Yards = totalBack9Yards;
+        this.overallPar = overallPar;
     }
 
     public long getId() {
@@ -98,14 +155,6 @@ public class MenTees {
 
     public void setId(long id) {
         this.id = id;
-    }
-
-    public MenTeesHoleYardages getMensTeesHolesYardages() {
-        return mensTeesHolesYardages;
-    }
-
-    public void setMensTeesHolesYardages(MenTeesHoleYardages mensTeesHolesYardages) {
-        this.mensTeesHolesYardages = mensTeesHolesYardages;
     }
 
     public Course getCourse() {
@@ -130,14 +179,6 @@ public class MenTees {
 
     public void setSlopeRating(double slopeRating) {
         this.slopeRating = slopeRating;
-    }
-
-    public long getTotalYards() {
-        return totalYards;
-    }
-
-    public void setTotalYards(long totalYards) {
-        this.totalYards = totalYards;
     }
 
     public int getHole1() {
@@ -284,32 +325,171 @@ public class MenTees {
         this.hole18 = hole18;
     }
 
-    public List<Integer> getFront9Pars() {
-        List<Integer> front9Pars = new ArrayList<>();
-        front9Pars.add(getHole1());
-        front9Pars.add(getHole2());
-        front9Pars.add(getHole3());
-        front9Pars.add(getHole4());
-        front9Pars.add(getHole5());
-        front9Pars.add(getHole6());
-        front9Pars.add(getHole7());
-        front9Pars.add(getHole8());
-        front9Pars.add(getHole9());
-        return front9Pars;
+    public int getHole1Yards() {
+        return hole1Yards;
     }
 
-    @NotNull
-    private List<Integer> getIntegers() {
-        List<Integer> back9Pars = new ArrayList<>();
-        back9Pars.add(getHole10());
-        back9Pars.add(getHole11());
-        back9Pars.add(getHole12());
-        back9Pars.add(getHole13());
-        back9Pars.add(getHole14());
-        back9Pars.add(getHole15());
-        back9Pars.add(getHole16());
-        back9Pars.add(getHole17());
-        back9Pars.add(getHole18());
-        return back9Pars;
+    public void setHole1Yards(int hole1Yards) {
+        this.hole1Yards = hole1Yards;
+    }
+
+    public int getHole2Yards() {
+        return hole2Yards;
+    }
+
+    public void setHole2Yards(int hole2Yards) {
+        this.hole2Yards = hole2Yards;
+    }
+
+    public int getHole3Yards() {
+        return hole3Yards;
+    }
+
+    public void setHole3Yards(int hole3Yards) {
+        this.hole3Yards = hole3Yards;
+    }
+
+    public int getHole4Yards() {
+        return hole4Yards;
+    }
+
+    public void setHole4Yards(int hole4Yards) {
+        this.hole4Yards = hole4Yards;
+    }
+
+    public int getHole5Yards() {
+        return hole5Yards;
+    }
+
+    public void setHole5Yards(int hole5Yards) {
+        this.hole5Yards = hole5Yards;
+    }
+
+    public int getHole6Yards() {
+        return hole6Yards;
+    }
+
+    public void setHole6Yards(int hole6Yards) {
+        this.hole6Yards = hole6Yards;
+    }
+
+    public int getHole7Yards() {
+        return hole7Yards;
+    }
+
+    public void setHole7Yards(int hole7Yards) {
+        this.hole7Yards = hole7Yards;
+    }
+
+    public int getHole8Yards() {
+        return hole8Yards;
+    }
+
+    public void setHole8Yards(int hole8Yards) {
+        this.hole8Yards = hole8Yards;
+    }
+
+    public int getHole9Yards() {
+        return hole9Yards;
+    }
+
+    public void setHole9Yards(int hole9Yards) {
+        this.hole9Yards = hole9Yards;
+    }
+
+    public int getHole10Yards() {
+        return hole10Yards;
+    }
+
+    public void setHole10Yards(int hole10Yards) {
+        this.hole10Yards = hole10Yards;
+    }
+
+    public int getHole11Yards() {
+        return hole11Yards;
+    }
+
+    public void setHole11Yards(int hole11Yards) {
+        this.hole11Yards = hole11Yards;
+    }
+
+    public int getHole12Yards() {
+        return hole12Yards;
+    }
+
+    public void setHole12Yards(int hole12Yards) {
+        this.hole12Yards = hole12Yards;
+    }
+
+    public int getHole13Yards() {
+        return hole13Yards;
+    }
+
+    public void setHole13Yards(int hole13Yards) {
+        this.hole13Yards = hole13Yards;
+    }
+
+    public int getHole14Yards() {
+        return hole14Yards;
+    }
+
+    public void setHole14Yards(int hole14Yards) {
+        this.hole14Yards = hole14Yards;
+    }
+
+    public int getHole15Yards() {
+        return hole15Yards;
+    }
+
+    public void setHole15Yards(int hole15Yards) {
+        this.hole15Yards = hole15Yards;
+    }
+
+    public int getHole16Yards() {
+        return hole16Yards;
+    }
+
+    public void setHole16Yards(int hole16Yards) {
+        this.hole16Yards = hole16Yards;
+    }
+
+    public int getHole17Yards() {
+        return hole17Yards;
+    }
+
+    public void setHole17Yards(int hole17Yards) {
+        this.hole17Yards = hole17Yards;
+    }
+
+    public int getHole18Yards() {
+        return hole18Yards;
+    }
+
+    public void setHole18Yards(int hole18Yards) {
+        this.hole18Yards = hole18Yards;
+    }
+
+    public long getTotalFront9Yards() {
+        return totalFront9Yards;
+    }
+
+    public void setTotalFront9Yards(long totalFront9Yards) {
+        this.totalFront9Yards = totalFront9Yards;
+    }
+
+    public long getTotalBack9Yards() {
+        return totalBack9Yards;
+    }
+
+    public void setTotalBack9Yards(long totalBack9Yards) {
+        this.totalBack9Yards = totalBack9Yards;
+    }
+
+    public int getOverallPar() {
+        return overallPar;
+    }
+
+    public void setOverallPar(int overallPar) {
+        this.overallPar = overallPar;
     }
 }
