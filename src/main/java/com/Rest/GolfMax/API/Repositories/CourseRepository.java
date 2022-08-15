c5313a87c1c8a5df4d04126a984d02eb4c1ac288
Olivier Charbonneau <ocharb@mac.com>
2022-08-14 18:53:34 -0700

    Changed package to Repositories

--- src/main/java/com/Rest/GolfMax/API/Repositories/CourseRepository.java
+++ src/main/java/com/Rest/GolfMax/API/Repositories/CourseRepository.java
@@ -1,9 +1,9 @@
-package com.Rest.GolfMax.API.Course;
+package com.Rest.GolfMax.API.Repositories;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.Rest.GolfMax.API.Course.Course;
+import com.Rest.GolfMax.API.Models.Course;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
