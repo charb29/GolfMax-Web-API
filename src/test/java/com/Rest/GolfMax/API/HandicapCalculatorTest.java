a9ecfe10aad9072c887135a15fb5819f126a746c
Olivier Charbonneau <ocharb@mac.com>
2022-08-14 22:11:00 -0700

    Added function to test if Calculator returns right result.

--- src/test/java/com/Rest/GolfMax/API/HandicapCalculatorTest.java
+++ src/test/java/com/Rest/GolfMax/API/HandicapCalculatorTest.java
@@ -1,8 +1,7 @@
 package com.Rest.GolfMax.API;
 
 
-import com.Rest.GolfMax.API.Models.HandicapCalculator;
-import com.Rest.GolfMax.API.Models.Score;
+import com.Rest.GolfMax.API.Models.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -55,20 +54,31 @@ public class HandicapCalculatorTest {
     }
 
     @Test
+    public void handicapIndexReturnsCorrectResult() {
+        List<Score> scores = new ArrayList<>();
+        scores.add(SCORE_1);
+        scores.add(SCORE_2);
+        scores.add(SCORE_3);
+        scores.add(SCORE_4);
+        scores.add(SCORE_5);
+        HandicapCalculator sut = new HandicapCalculator();
+        double expectResult = -8;
+        sut.setHandicapIndex(scores);
+        double actualResult = sut.getHandicapIndex();
+
+        assertEquals(expectResult, actualResult);
+    }
+
+    @Test
     public void userWithLessThan_5_ScoresReturns_0_ForHandicapIndex() {
-        HandicapCalculator sut = new HandicapCalculator(); // sut = system under test
-        List<Score> scores = new ArrayList<Score>();
-        Score SCORE_1 = new Score();
-        Score SCORE_2 = new Score();
-        Score SCORE_3 = new Score();
-        SCORE_1.setUserScore(101); SCORE_1.setCourseRating(79.1); SCORE_1.setSlopeRating(121);
-        SCORE_2.setUserScore(130); SCORE_2.setCourseRating(61); SCORE_2.setSlopeRating(103);
-        SCORE_3.setUserScore(130); SCORE_3.setCourseRating(54.8); SCORE_3.setSlopeRating(86);
+        HandicapCalculator sut = new HandicapCalculator();
+        List<Score> scores = new ArrayList<>();
         scores.add(SCORE_1);
         scores.add(SCORE_2);
         scores.add(SCORE_3);
         double expectedResult = 0;
 
+        sut.setHandicapIndex(scores);
         double actualResult = sut.getHandicapIndex();
 
         assertEquals(expectedResult, actualResult, 0.0001);
@@ -77,7 +87,7 @@ public class HandicapCalculatorTest {
     @Test
     public void averageScoreDifferentialWithLessThan_5_ResultsReturns_0() {
         HandicapCalculator sut = new HandicapCalculator();
-        List<Double> averageScoreDifferentials = new ArrayList<Double>();
+        List<Double> averageScoreDifferentials = new ArrayList<>();
         averageScoreDifferentials.add(4.5);
         averageScoreDifferentials.add(2.3);
         averageScoreDifferentials.add(5.7);
@@ -91,7 +101,7 @@ public class HandicapCalculatorTest {
     @Test
     public void averageScoreDifferentialWithLessThan_5_ResultsReturns_0_HandicapIndex() {
         HandicapCalculator sut = new HandicapCalculator();
-        List<Double> averageScoreDifferentials = new ArrayList<Double>();
+        List<Double> averageScoreDifferentials = new ArrayList<>();
         averageScoreDifferentials.add(2.3);
         averageScoreDifferentials.add(4.2);
         averageScoreDifferentials.add(3.4);
