152153b5680c2a814e6238fab96adf0ce0363d91
Olivier Charbonneau <ocharb@mac.com>
2022-08-14 22:10:29 -0700

    Added Tee, Course, Score and User objects

--- src/test/java/com/Rest/GolfMax/API/HandicapCalculatorTest.java
+++ src/test/java/com/Rest/GolfMax/API/HandicapCalculatorTest.java
@@ -12,6 +12,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HandicapCalculatorTest {
 
+    private final ChampionshipTees CHAMPIONSHIP_TEES = new ChampionshipTees(1, 65.4, 113,
+            4, 3, 3, 4, 3, 3, 4, 3, 4, 4, 3,
+            3, 3, 3, 4, 4, 3, 3, 357, 213,
+            190, 371, 103, 166, 316, 139, 371,
+            439, 182, 71, 82, 121, 332, 404,
+            156, 179, 2226, 1966, 61);
+    private final MenTees MENS_TEES = new MenTees(1, 63.6, 107, 4, 3, 3,
+            4, 3, 3, 4, 3, 4, 4, 3, 3, 3, 3,
+            4, 4, 3, 3, 336, 202, 179, 365,
+            85, 141, 305, 129, 360, 454, 166,
+            71, 74, 117, 311, 389, 145, 169,
+            2102, 1866, 61);
+    private final WomenTees WOMEN_TEES = new WomenTees(1, 62.1, 104, 4, 4, 3,
+            4, 3, 3, 4, 3, 5, 5, 3, 3, 3, 3,
+            4, 4, 3, 3, 312, 184, 169, 356, 68,
+            124, 288, 114, 340, 393, 147, 71,
+            73, 117, 277, 375, 112, 161,
+            1955, 1726, 64);
+    private final Course COURSE = new Course(1, CHAMPIONSHIP_TEES, MENS_TEES, WOMEN_TEES,
+            "Vista Valencia Golf Course");
+    private final User USER = new User(1, "Olivier", "password", "email@email.com");
+    private final Score SCORE_1 = new Score(1, USER, COURSE, 69,
+            CHAMPIONSHIP_TEES.getCourseRating(), CHAMPIONSHIP_TEES.getSlopeRating());
+        private final Score SCORE_2 = new Score(2, USER, COURSE, 72,
+            CHAMPIONSHIP_TEES.getCourseRating(), CHAMPIONSHIP_TEES.getSlopeRating());
+    private final Score SCORE_3 = new Score(3, USER, COURSE, 85,
+            MENS_TEES.getCourseRating(), MENS_TEES.getSlopeRating());
+    private final Score SCORE_4 = new Score(4, USER, COURSE, 56,
+            MENS_TEES.getCourseRating(), MENS_TEES.getSlopeRating());
+    private final Score SCORE_5 = new Score(5, USER, COURSE, 61,
+            CHAMPIONSHIP_TEES.getCourseRating(), CHAMPIONSHIP_TEES.getSlopeRating());
+
     @Test
     public void handicapIndexReturnsOnly_1_DecimalPlace() {
         HandicapCalculator sut = new HandicapCalculator();
