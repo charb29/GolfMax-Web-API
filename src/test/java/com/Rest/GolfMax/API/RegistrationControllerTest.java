da483ce3f5d8932617a06f7c528a83960e708445
Olivier Charbonneau <ocharb@mac.com>
2022-08-22 16:05:01 -0700

    testing controller endpoints

--- src/test/java/com/Rest/GolfMax/API/RegistrationControllerTest.java
+++ src/test/java/com/Rest/GolfMax/API/RegistrationControllerTest.java
@@ -1,12 +1,13 @@
 package com.Rest.GolfMax.API;
 
 import com.Rest.GolfMax.API.Controllers.RegistrationController;
+import com.Rest.GolfMax.API.DTOs.UserDto;
 import com.Rest.GolfMax.API.Models.User;
-import com.Rest.GolfMax.API.Repositories.UserRepository;
-import com.Rest.GolfMax.API.Services.UserService;
+import com.Rest.GolfMax.API.Services.Interfaces.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,8 +15,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.Matchers.*;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest(RegistrationController.class)
@@ -23,26 +25,28 @@ public class RegistrationControllerTest {
     @Autowired
     private MockMvc mockMvc;
     @Autowired
-    private ObjectMapper mapper;
-    @MockBean
-    private UserRepository userRepository;
+    private ObjectMapper objectMapper;
+    @Autowired
+    private ModelMapper modelMapper;
     @MockBean
     private UserService userService;
 
-    User USER_1 = new User(1, "Dvean", "password", "dvean@gmail.com");
-
     @Test
-    public void register() throws Exception {
-        Mockito.when(userRepository.save(USER_1)).thenReturn(USER_1);
+    public void registerUser_returns_HTTP_CREATED() throws Exception {
+        User newUser = new User();
+        newUser.setUsername("Olivier");
+        newUser.setPassword("password");
+        newUser.setEmail("email");
 
-        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/users/account")
+        Mockito.when(userService.createUser(newUser)).thenReturn(newUser);
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post("/users/auth/signup")
                 .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content(this.mapper.writeValueAsString(USER_1));
+                .contentType(objectMapper.writeValueAsString(newUser));
 
-        mockMvc.perform(mockRequest)
+        mockMvc.perform(request)
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$", notNullValue()))
-                .andExpect(jsonPath("$.username", is("Dvean")));
+                .andDo(print());
     }
 }
