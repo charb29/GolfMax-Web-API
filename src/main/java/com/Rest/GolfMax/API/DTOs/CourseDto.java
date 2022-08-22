6307d1ec6e637171924b79abf17fa7d5078c24f6
Olivier Charbonneau <ocharb@mac.com>
2022-08-21 18:00:32 -0700

    Created DTOs & moved to DTOs package

---
+++ src/main/java/com/Rest/GolfMax/API/DTOs/CourseDto.java
@@ -0,0 +1,41 @@
+package com.Rest.GolfMax.API.DTOs;
+
+import java.util.List;
+
+public class CourseDto {
+    private long id;
+    private List<HoleLayoutDto> holeLayoutDto;
+    private String courseName;
+
+    public CourseDto() {}
+
+    public CourseDto(long id, List<HoleLayoutDto> holeLayoutDto, String courseName) {
+        this.id = id;
+        this.holeLayoutDto = holeLayoutDto;
+        this.courseName = courseName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<HoleLayoutDto> getHoleLayoutDto() {
+        return holeLayoutDto;
+    }
+
+    public void setHoleLayoutDto(List<HoleLayoutDto> holeLayoutDto) {
+        this.holeLayoutDto = holeLayoutDto;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+}
