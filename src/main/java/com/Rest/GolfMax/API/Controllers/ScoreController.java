6dcaeca43d2d7a0c13d7094116edcadec26d7e49
Olivier Charbonneau <ocharb@mac.com>
2022-08-21 18:03:19 -0700

    Implemented score service interface

--- src/main/java/com/Rest/GolfMax/API/Controllers/ScoreController.java
+++ src/main/java/com/Rest/GolfMax/API/Controllers/ScoreController.java
@@ -1,56 +1,83 @@
 package com.Rest.GolfMax.API.Controllers;
 
+import com.Rest.GolfMax.API.DTOs.ScoreDto;
 import com.Rest.GolfMax.API.Models.Score;
-import com.Rest.GolfMax.API.Services.ScoreService;
+import com.Rest.GolfMax.API.Services.Interfaces.ScoreService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/scores")
 public class ScoreController {
-    
     @Autowired
-    ScoreService scoreService;
+    private ModelMapper modelMapper;
 
-    @GetMapping("")
-    public List<Score> getAllScores() {
-        return scoreService.listAllScores();
+    private final ScoreService SCORE_SERVICE;
+
+    public ScoreController(ScoreService scoreService) {
+        super();
+        this.SCORE_SERVICE = scoreService;
+    }
+
+    @GetMapping
+    public List<ScoreDto> getAllScores() {
+        return SCORE_SERVICE.getAllScores()
+                .stream()
+                .map(post -> modelMapper.map(post, ScoreDto.class))
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Score> getScoresById(@PathVariable Long id) {
+    public ResponseEntity<ScoreDto> getScoresById(@PathVariable long id) {
         try {
-            Score score = scoreService.getScoreById(id);
-            return new ResponseEntity<Score> (score, HttpStatus.OK);
+            Score score = SCORE_SERVICE.getScoreById(id);
+            ScoreDto scoreResponse = modelMapper.map(score, ScoreDto.class);
+            return new ResponseEntity<>(scoreResponse, HttpStatus.OK);
         } catch (NoSuchElementException e) {
-            return new ResponseEntity<Score> (HttpStatus.NOT_FOUND);
+            return new ResponseEntity<> (HttpStatus.NOT_FOUND);
         }
     }
 
-    @GetMapping("/user/{id}")
-    public List<Score> getScoresByUserId(@PathVariable long id, Sort sort) {
-        return scoreService.getScoresByUserId(id, Sort.by("userScore").ascending());
+    @GetMapping("/users/{id}/user-scores")
+    public ResponseEntity<List<ScoreDto>> getScoresByUserId(@PathVariable long id) {
+        try {
+            List<Score> scores = SCORE_SERVICE.getScoresByUserId(id);
+            ScoreDto scoreResponse = modelMapper.map(scores, ScoreDto.class);
+            return new ResponseEntity<>(Arrays.asList(scoreResponse), HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
-    @PostMapping("/add-score")
-    public ResponseEntity<Score> addScore(@RequestBody Score score) {
-            scoreService.saveScore(score);
-            return new ResponseEntity<>(score, HttpStatus.CREATED);
+    @PostMapping("/users")
+    public ResponseEntity<ScoreDto> addScore(@RequestBody ScoreDto scoreDto) {
+        Score scoreRequest = modelMapper.map(scoreDto, Score.class);
+        Score score = SCORE_SERVICE.createScore(scoreRequest);
+        ScoreDto scoreResponse = modelMapper.map(score, ScoreDto.class);
+        return new ResponseEntity<>(scoreResponse, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteScore(@PathVariable Long id) {
-        scoreService.deleteScore(id);
+    public void deleteScore(@PathVariable long id) {
+        SCORE_SERVICE.deleteScore(id);
     }
 
-    @GetMapping("/course/{id}")
-    public List<Score> getScoresByCourseId(@PathVariable long id, Sort sort) {
-        return scoreService.getScoreByCourseId(id, Sort.by("userScore").ascending());
+    @GetMapping("/user-scores/courses/{id}")
+    public ResponseEntity<List<ScoreDto>> getScoresByCourseId(@PathVariable long id) {
+        try {
+            List<Score> scores = SCORE_SERVICE.getScoresByCourseId(id);
+            ScoreDto scoreResponse = modelMapper.map(scores, ScoreDto.class);
+            return new ResponseEntity<>(Arrays.asList(scoreResponse), HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
