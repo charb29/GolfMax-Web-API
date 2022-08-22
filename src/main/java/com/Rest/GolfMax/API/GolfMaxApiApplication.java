54ab88fd7c38ea4309823faa9f066bd46f196708
Olivier Charbonneau <ocharb@mac.com>
2022-08-21 17:59:18 -0700

    Added @Bean ModelMapper to map DTOs

--- src/main/java/com/Rest/GolfMax/API/GolfMaxApiApplication.java
+++ src/main/java/com/Rest/GolfMax/API/GolfMaxApiApplication.java
@@ -1,10 +1,17 @@
 package com.Rest.GolfMax.API;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GolfMaxApiApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GolfMaxApiApplication.class, args);
