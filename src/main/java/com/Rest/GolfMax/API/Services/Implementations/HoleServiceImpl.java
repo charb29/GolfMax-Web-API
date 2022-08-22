b1e37d2b0b7fe0884df1cadec95d11db3129d39e
Olivier Charbonneau <ocharb@mac.com>
2022-08-22 16:03:55 -0700

    Implemented HoleService interface

---
+++ src/main/java/com/Rest/GolfMax/API/Services/Implementations/HoleServiceImpl.java
@@ -0,0 +1,26 @@
+package com.Rest.GolfMax.API.Services.Implementations;
+
+import com.Rest.GolfMax.API.Models.Hole;
+import com.Rest.GolfMax.API.Repositories.HoleRepository;
+import com.Rest.GolfMax.API.Services.Interfaces.HoleService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Service
+@Transactional
+@RequestScope
+public class HoleServiceImpl implements HoleService {
+
+    private final HoleRepository HOLE_REPOSITORY;
+
+    public HoleServiceImpl(HoleRepository holeRepository) {
+        super();
+        this.HOLE_REPOSITORY = holeRepository;
+    }
+
+    @Override
+    public Hole createHole(Hole hole) {
+        return HOLE_REPOSITORY.save(hole);
+    }
+}
