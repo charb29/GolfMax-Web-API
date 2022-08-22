4dd29033e2f5b18e391609868cd204826141e2e9
Olivier Charbonneau <ocharb@mac.com>
2022-08-22 16:04:44 -0700

    added query to get stats by user id in ascending order

--- src/main/java/com/Rest/GolfMax/API/Repositories/PlayerStatisticsRepository.java
+++ src/main/java/com/Rest/GolfMax/API/Repositories/PlayerStatisticsRepository.java
@@ -1,11 +1,14 @@
 package com.Rest.GolfMax.API.Repositories;
 
 import com.Rest.GolfMax.API.Models.PlayerStatistics;
+import com.Rest.GolfMax.API.Models.Score;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PlayerStatisticsRepository extends JpaRepository<PlayerStatistics, Long> {
 
-    PlayerStatistics findByUserId(long id);
+    List<PlayerStatistics> findByUserIdOrderByUserScoresAsc(long id);
 }
