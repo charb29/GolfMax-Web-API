bafd207f1cc1e5e36436d478ab4d7788ae3fe47d
charb29 <ocharb@mac.com>
2022-08-24 16:18:44 -0700

    Test to test PostMapping endpoint in RegistrationController

--- src/test/java/com/Rest/GolfMax/API/RegistrationControllerTest.java
+++ src/test/java/com/Rest/GolfMax/API/RegistrationControllerTest.java
@@ -2,7 +2,6 @@ package com.Rest.GolfMax.API;
 
 import com.Rest.GolfMax.API.Controllers.RegistrationController;
 import com.Rest.GolfMax.API.Models.User;
-import com.Rest.GolfMax.API.Repositories.UserRepository;
 import com.Rest.GolfMax.API.Services.Interfaces.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -15,8 +14,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(RegistrationController.class)
@@ -31,14 +31,23 @@ public class RegistrationControllerTest {
     @Test
     public void registerUser_returns_HTTP_CREATED() throws Exception {
         User user = new User();
+        user.setUsername("Olivier");
+        user.setPassword("password");
+        user.setEmail("email");
+
         Mockito.when(userService.createUser(Mockito.any(User.class))).thenReturn(user);
 
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders
                 .post("/users/auth/signup")
                 .contentType(MediaType.APPLICATION_JSON)
-                .contentType(objectMapper.writeValueAsString(user));
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(user));
 
         mockMvc.perform(request)
-                .andExpect(status().isCreated());
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.username", is("Olivier")))
+                .andExpect(jsonPath("$.email", is("email")))
+                .andExpect(jsonPath("$.password", is("password")));
     }
 }
