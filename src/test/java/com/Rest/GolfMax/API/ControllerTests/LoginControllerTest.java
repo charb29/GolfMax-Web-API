1e05f9514d3a6bc063311a7cdd3a481e06a7898a
charb29 <ocharb@mac.com>
2022-08-24 17:50:01 -0700

    Testing login PostMapping endpoint in LoginController

--- src/test/java/com/Rest/GolfMax/API/ControllerTests/LoginControllerTest.java
+++ src/test/java/com/Rest/GolfMax/API/ControllerTests/LoginControllerTest.java
@@ -1,32 +1,62 @@
 package com.Rest.GolfMax.API.ControllerTests;
+
 import com.Rest.GolfMax.API.Controllers.LoginController;
+import com.Rest.GolfMax.API.DTOs.UserDto;
 import com.Rest.GolfMax.API.Models.User;
-import com.Rest.GolfMax.API.Services.Implementations.UserServiceImpl;
+import com.Rest.GolfMax.API.Services.Interfaces.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(LoginController.class)
 public class LoginControllerTest {
     @Autowired
     private MockMvc mockMvc;
     @Autowired
-    private ObjectMapper mapper;
+    private ObjectMapper objectMapper;
+    @Autowired
+    private ModelMapper modelMapper;
     @MockBean
-    private UserServiceImpl service;
+    private UserService userService;
 
-    private final User EXISTING_USER = new User(1, "Olivier", "password", "olivier@gmail.com");;
-    private final User INVALID_USER = new User();
+    @Test
+    public void successful_login_returns_HTTP_OK() throws Exception {
+        User user = new User();
+        user.setId(1);
+        user.setUsername("Olivier");
+        user.setPassword("password");
+        user.setEmail("olivier@gmail.com");
 
+        Mockito.when(userService.getUserData(Mockito.any(User.class))).thenReturn(user);
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/users/auth/signin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(user));
+
+        mockMvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.username", is("Olivier")))
+                .andExpect(jsonPath("$.password", is("password")))
+                .andExpect(jsonPath("$.email", is("olivier@gmail.com")));
+    }
 
 }
