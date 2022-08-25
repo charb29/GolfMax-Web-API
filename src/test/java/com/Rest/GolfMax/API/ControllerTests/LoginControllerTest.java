34b5fcc198d80873530eb09ad1142666c3300a92
charb29 <ocharb@mac.com>
2022-08-24 17:50:25 -0700

    Removed unused import (ModelMapper)

--- src/test/java/com/Rest/GolfMax/API/ControllerTests/LoginControllerTest.java
+++ src/test/java/com/Rest/GolfMax/API/ControllerTests/LoginControllerTest.java
@@ -1,13 +1,11 @@
 package com.Rest.GolfMax.API.ControllerTests;
 
 import com.Rest.GolfMax.API.Controllers.LoginController;
-import com.Rest.GolfMax.API.DTOs.UserDto;
 import com.Rest.GolfMax.API.Models.User;
 import com.Rest.GolfMax.API.Services.Interfaces.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -28,8 +26,6 @@ public class LoginControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
-    @Autowired
-    private ModelMapper modelMapper;
     @MockBean
     private UserService userService;
 
