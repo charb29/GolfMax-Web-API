08665ccd8e49076850a1672d1f129677a86e307e
charb29 <ocharb@mac.com>
2022-08-26 18:02:51 -0700

    changed getUserById to Optional<User>

--- src/main/java/com/Rest/GolfMax/API/Services/Interfaces/UserService.java
+++ src/main/java/com/Rest/GolfMax/API/Services/Interfaces/UserService.java
@@ -3,12 +3,13 @@ package com.Rest.GolfMax.API.Services.Interfaces;
 import com.Rest.GolfMax.API.Models.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
     List<User> getAllUsers();
     User updateUser(User user, long id);
     User createUser(User user);
-    User getUserById(long id);
+    Optional<User> getUserById(long id);
     void deleteUser(long id);
     boolean userExists(User user);
     User getUserData(User user);
