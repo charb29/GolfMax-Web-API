5437d2814bfc257aa336b82f5083f9be9f6430f7
Olivier Charbonneau <ocharb@mac.com>
2022-08-27 21:13:01 -0700

    imported Optional

--- src/main/java/com/Rest/GolfMax/API/Services/Interfaces/ScoreService.java
+++ src/main/java/com/Rest/GolfMax/API/Services/Interfaces/ScoreService.java
@@ -4,11 +4,12 @@ import com.Rest.GolfMax.API.Models.Score;
 
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ScoreService {
     List<Score> getAllScores();
     Score createScore(Score score);
-    Score getScoreById(long id);
+    Optional<Score>getScoreById(long id);
     void deleteScore(long id);
     List<Score> getScoresByUserId(long id);
     List<Score> getScoresByCourseId(long id);
