334db19e4021657168f1c418bc28c1445effdbea
Olivier Charbonneau <ocharb@mac.com>
2022-09-05 16:57:03 -0700

    Added extra space between class declaration and HoleRepository initialization

--- src/test/java/com/Rest/GolfMax/API/ServiceTests/HoleServiceTest.java
+++ src/test/java/com/Rest/GolfMax/API/ServiceTests/HoleServiceTest.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class HoleServiceTest {
+
     @Mock
     private HoleRepository holeRepository;
     @InjectMocks
