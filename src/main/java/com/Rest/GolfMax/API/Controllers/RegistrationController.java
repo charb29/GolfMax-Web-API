c88aeb1ec5a573886f4ac4ac483953a313ad09aa
Olivier Charbonneau <ocharb@mac.com>
2022-09-05 16:59:07 -0700

    Replaced usage of UserDto with User entity

--- src/main/java/com/Rest/GolfMax/API/Controllers/RegistrationController.java
+++ src/main/java/com/Rest/GolfMax/API/Controllers/RegistrationController.java
@@ -1,10 +1,7 @@
 package com.Rest.GolfMax.API.Controllers;
 
-import com.Rest.GolfMax.API.DTOs.UserDto;
 import com.Rest.GolfMax.API.Models.User;
 import com.Rest.GolfMax.API.Services.Interfaces.UserService;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users/auth")
 public class RegistrationController {
-    @Autowired
-    private ModelMapper modelMapper;
+
     private final UserService USER_SERVICE;
 
     public RegistrationController(UserService userService) {
@@ -25,14 +21,11 @@ public class RegistrationController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<UserDto> registerUser(@RequestBody UserDto userDto) {
-        User userRequest = modelMapper.map(userDto, User.class);
-
-        if (USER_SERVICE.userExists(userRequest))
+    public ResponseEntity<User> registerUser(@RequestBody User userRequest) {
+        if (USER_SERVICE.userExists(userRequest)) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-
-        User user = USER_SERVICE.createUser(userRequest);
-        UserDto userResponse = modelMapper.map(user, UserDto.class);
+        }
+        User userResponse = USER_SERVICE.createUser(userRequest);
         return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
     }
 }
