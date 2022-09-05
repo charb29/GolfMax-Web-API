76332eb2a8aefcae4bc785fb4b12fb443961ab79
Olivier Charbonneau <ocharb@mac.com>
2022-09-05 16:59:27 -0700

    Removed usage of 'Optional'

--- src/main/java/com/Rest/GolfMax/API/Services/Interfaces/ScoreService.java
+++ src/main/java/com/Rest/GolfMax/API/Services/Interfaces/ScoreService.java
@@ -4,12 +4,12 @@ import com.Rest.GolfMax.API.Models.Score;
 
 
 import java.util.List;
-import java.util.Optional;
+
 
 public interface ScoreService {
     List<Score> getAllScores();
     Score createScore(Score score);
-    Optional<Score>getScoreById(Long id);
+    Score getScoreById(Long id);
     void deleteScore(Long id);
     List<Score> getScoresByUserId(Long id);
     List<Score> getScoresByCourseId(Long id);
