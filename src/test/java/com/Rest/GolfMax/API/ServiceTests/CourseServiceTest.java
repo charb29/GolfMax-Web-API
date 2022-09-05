0004f4d3f0af09b6eb05ae2ceb651e40e2794cbc
Olivier Charbonneau <ocharb@mac.com>
2022-09-05 16:56:34 -0700

    Added extra space between class declaration and CourseRepository initialization

--- src/test/java/com/Rest/GolfMax/API/ServiceTests/CourseServiceTest.java
+++ src/test/java/com/Rest/GolfMax/API/ServiceTests/CourseServiceTest.java
@@ -21,6 +21,7 @@ import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CourseServiceTest {
+
     @Mock
     private CourseRepository courseRepository;
     @InjectMocks
