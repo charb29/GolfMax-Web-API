91c5349c61f45f32169fae997d65b92193347af9
Olivier Charbonneau <ocharb@mac.com>
2022-09-05 16:58:11 -0700

    Replaced usage of UserDto with User entity

--- src/main/java/com/Rest/GolfMax/API/Controllers/LoginController.java
+++ src/main/java/com/Rest/GolfMax/API/Controllers/LoginController.java
@@ -1,12 +1,7 @@
 package com.Rest.GolfMax.API.Controllers;
 
-import com.Rest.GolfMax.API.DTOs.UserDto;
 import com.Rest.GolfMax.API.Models.User;
-import com.Rest.GolfMax.API.Services.Implementations.UserServiceImpl;
 import com.Rest.GolfMax.API.Services.Interfaces.UserService;
-import org.jetbrains.annotations.NotNull;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/users/auth")
 public class LoginController {
-    @Autowired
-    private ModelMapper modelMapper;
+
     private final UserService USER_SERVICE;
 
     public LoginController(UserService userService) {
@@ -24,12 +18,9 @@ public class LoginController {
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<UserDto> login(@RequestBody UserDto userDto) {
-        User userRequest = modelMapper.map(userDto, User.class);
-        UserDto userResponse = modelMapper.map(userRequest, UserDto.class);
-
-        if (USER_SERVICE.getUserData(userRequest) != null)
-            return new ResponseEntity<>(userResponse, HttpStatus.OK);
+    public ResponseEntity<User> login(@RequestBody User user) {
+        if (USER_SERVICE.getUserData(user) != null)
+            return new ResponseEntity<>(user, HttpStatus.OK);
         else
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
