06742ca294888442913c58fc8e039655170b798d
charb29 <ocharb@mac.com>
2022-09-09 18:09:16 -0700

    Added method to encryptPassword & validatePassword

--- src/main/java/com/Rest/GolfMax/API/Services/Interfaces/UserService.java
+++ src/main/java/com/Rest/GolfMax/API/Services/Interfaces/UserService.java
@@ -2,15 +2,20 @@ package com.Rest.GolfMax.API.Services.Interfaces;
 
 import com.Rest.GolfMax.API.Models.User;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
     List<User> getAllUsers();
-    User updateUser(User user, Long id);
-    User createUser(User user);
+    User updateUser(User user, Long id) throws NoSuchAlgorithmException, InvalidKeySpecException;
+    User createUser(User user) throws NoSuchAlgorithmException, InvalidKeySpecException;
     Optional<User> getUserById(Long id);
     void deleteUser(Long id);
-    boolean isValidLoginRequest(User user);
+    boolean isValidLoginRequest(User user) throws NoSuchAlgorithmException, InvalidKeySpecException;
     boolean isValidRegistrationRequest(String username, String email);
+    String encryptPassword(String password) throws NoSuchAlgorithmException, InvalidKeySpecException;
+    boolean isValidPassword(String originalPassword, String storedPassword) throws NoSuchAlgorithmException,
+            InvalidKeySpecException;
 }
