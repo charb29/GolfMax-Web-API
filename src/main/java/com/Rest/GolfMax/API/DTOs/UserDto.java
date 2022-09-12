1d7be16813686b5acbcdc8eef339762eb68b8d34
Olivier Charbonneau <ocharb@mac.com>
2022-09-11 22:57:11 -0700

    Added firstName and lastName variables + getter and setters

--- src/main/java/com/Rest/GolfMax/API/DTOs/UserDto.java
+++ src/main/java/com/Rest/GolfMax/API/DTOs/UserDto.java
@@ -2,16 +2,35 @@ package com.Rest.GolfMax.API.DTOs;
 
 public class UserDto {
 
+    private String firstName;
+    private String lastName;
     private String username;
     private String password;
     private String email;
 
     public UserDto() {}
 
-    public UserDto(String username, String password, String email) {
+    public UserDto(String firstName, String lastName, String username, String password, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.username = username;
-        this.password = password;
         this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getUsername() {
