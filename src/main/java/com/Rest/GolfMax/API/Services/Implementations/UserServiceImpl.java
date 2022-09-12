d88314bd51f82c8806110b0103f1b14da61c39f5
Olivier Charbonneau <ocharb@mac.com>
2022-09-11 23:00:33 -0700

    Methods to register and validate verification code

--- src/main/java/com/Rest/GolfMax/API/Services/Implementations/UserServiceImpl.java
+++ src/main/java/com/Rest/GolfMax/API/Services/Implementations/UserServiceImpl.java
@@ -2,19 +2,27 @@ package com.Rest.GolfMax.API.Services.Implementations;
 
 import com.Rest.GolfMax.API.Models.User;
 import com.Rest.GolfMax.API.Repositories.UserRepository;
-import com.Rest.GolfMax.API.Security.Email;
-import com.Rest.GolfMax.API.Security.Password;
+import com.Rest.GolfMax.API.Security.PasswordSecurity;
 import com.Rest.GolfMax.API.Services.Interfaces.UserService;
+import net.bytebuddy.utility.RandomString;
 import org.apache.velocity.exception.ResourceNotFoundException;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.RequestScope;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.transaction.Transactional;
+import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 
 @Service
@@ -22,13 +30,16 @@ import java.util.Optional;
 @RequestScope
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository USER_REPOSITORY;
-    private final Password PASSWORD = new Password();
-    private final Email EMAIL = new Email();
 
-    public UserServiceImpl(UserRepository USER_REPOSITORY) {
+    private final JavaMailSender MAIL_SENDER;
+    private final UserRepository USER_REPOSITORY;
+    private final PasswordSecurity PASSWORD_SECURITY = new PasswordSecurity();
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, JavaMailSender mailSender) {
         super();
-        this.USER_REPOSITORY = USER_REPOSITORY;
+        this.USER_REPOSITORY = userRepository;
+        this.MAIL_SENDER = mailSender;
     }
 
     @Override
@@ -41,7 +52,9 @@ public class UserServiceImpl implements UserService {
         User userResponse = new User();
         userResponse.setUsername(userRequest.getUsername());
         userResponse.setEmail(userRequest.getEmail());
-        userResponse.setPassword(PASSWORD.getEncrypted(userRequest.getPassword()));
+        userResponse.setPassword(PASSWORD_SECURITY.getEncrypted(userRequest.getPassword()));
+        userResponse.setFirstName(userRequest.getFirstName());
+        userResponse.setLastName(userRequest.getLastName());
         USER_REPOSITORY.save(userResponse);
 
         return userResponse;
@@ -64,7 +77,7 @@ public class UserServiceImpl implements UserService {
                 .orElseThrow(() -> new ResourceNotFoundException("id"));
         updatedUser.setUsername(userRequest.getUsername());
         updatedUser.setEmail(userRequest.getEmail());
-        updatedUser.setPassword(PASSWORD.getEncrypted(userRequest.getPassword()));
+        updatedUser.setPassword(PASSWORD_SECURITY.getEncrypted(userRequest.getPassword()));
         return USER_REPOSITORY.save(updatedUser);
     }
 
@@ -79,13 +92,45 @@ public class UserServiceImpl implements UserService {
             return false;
         }
 
-        return PASSWORD.isValid(password, encryptedPassword)
+        return PASSWORD_SECURITY.isValid(password, encryptedPassword)
                 && USER_REPOSITORY.existsByUsername(username);
     }
 
     @Override
     public boolean isValidRegistrationRequest(String username, String email) {
-        return !isValidUsername(username) && !isValidEmail(email) && EMAIL.isValidFormat(email);
+        return !isValidUsername(username) && !isValidEmail(email) && isValidFormat(email);
+    }
+
+    @Override
+    public User registerUser(User userRequest, String siteURL)
+            throws UnsupportedEncodingException, MessagingException,
+            NoSuchAlgorithmException, InvalidKeySpecException {
+        User savedUser = new User();
+        savedUser.setPassword(PASSWORD_SECURITY.getEncrypted(userRequest.getPassword()));
+        savedUser.setVerificationCode(RandomString.make(64));
+        savedUser.setEmail(userRequest.getEmail());
+        savedUser.setEnabled(false);
+        savedUser.setUsername(userRequest.getUsername());
+        savedUser.setFirstName(userRequest.getFirstName());
+        savedUser.setLastName(userRequest.getLastName());
+
+        User userResponse = USER_REPOSITORY.save(savedUser);
+        sendVerificationEmail(userResponse, siteURL);
+        return userResponse;
+    }
+
+    @Override
+    public boolean isValidVerificationCode(String verificationCode) {
+        User user = USER_REPOSITORY.findByVerificationCode(verificationCode);
+        if (user == null || user.isEnabled()) {
+            return false;
+        }
+        else {
+            user.setVerificationCode(null);
+            user.setEnabled(true);
+            USER_REPOSITORY.save(user);
+            return true;
+        }
     }
 
     private boolean isValidUsername(String username) {
@@ -96,4 +141,43 @@ public class UserServiceImpl implements UserService {
         return USER_REPOSITORY.existsByEmail(email);
     }
 
+    private boolean isValidFormat(String email) {
+        String regexPattern = "^[a-zA-Z\\d_+&*-]+(?:\\.[a-zA-Z\\d_+&*-]+)*@(?:[a-zA-Z\\d-]+\\.)+[a-zA-Z]{2,7}$";
+        Pattern pattern = Pattern.compile(regexPattern);
+        if (email == null) {
+            return false;
+        }
+        return pattern.matcher(email).matches();
+    }
+
+    private void sendVerificationEmail(User user, String siteURL)
+            throws MessagingException, UnsupportedEncodingException {
+        String recipient = user.getEmail();
+        String sender = "golfmax.user.services@gmail.com";
+        String companyName = "Golf Max";
+        String emailSubject = "Verify Your GolfMax Account";
+        String emailContent = "Hey there [[name]]!,<br>"
+                + "You're receiving this email because you recently signed up to GolfMax. To complete the"
+                + " registration process, hit the link below to verify your account:<br>"
+                + "<h3><a href=\"[[URL]]\" target=\"_self\">VERIFY</a></h3>"
+                + "Thank you,<br>"
+                + companyName;
+
+        MimeMessage message = MAIL_SENDER.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(message);
+
+        messageHelper.setFrom(sender, companyName);
+        messageHelper.setTo(recipient);
+        messageHelper.setSubject(emailSubject);
+
+        emailContent = emailContent.replace("[[name]]", user.getFirstName()
+                + " "
+                + user.getLastName());
+
+        String verifyURL = siteURL + "/verify?code=" + user.getVerificationCode();
+        emailContent = emailContent.replace("[[URL]]", verifyURL);
+        messageHelper.setText(emailContent, true);
+
+        MAIL_SENDER.send(message);
+    }
 }
--- src/main/java/com/Rest/GolfMax/API/Services/Interfaces/UserService.java
+++ src/main/java/com/Rest/GolfMax/API/Services/Interfaces/UserService.java
@@ -2,6 +2,8 @@ package com.Rest.GolfMax.API.Services.Interfaces;
 
 import com.Rest.GolfMax.API.Models.User;
 
+import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.List;
@@ -15,4 +17,8 @@ public interface UserService {
     void deleteUser(Long id);
     boolean isValidLoginRequest(User user) throws NoSuchAlgorithmException, InvalidKeySpecException;
     boolean isValidRegistrationRequest(String username, String email);
+    User registerUser(User user, String siteURL)
+            throws UnsupportedEncodingException, MessagingException,
+            NoSuchAlgorithmException, InvalidKeySpecException;
+    boolean isValidVerificationCode(String verificationCode);
 }
