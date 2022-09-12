dd5aea187adb38a0bfe80b24af67381089f2b24e
Olivier Charbonneau <ocharb@mac.com>
2022-09-11 22:55:56 -0700

    Added method to verify user and send a validation email

--- src/main/java/com/Rest/GolfMax/API/Controllers/RegistrationController.java
+++ src/main/java/com/Rest/GolfMax/API/Controllers/RegistrationController.java
@@ -5,17 +5,19 @@ import com.Rest.GolfMax.API.Models.User;
 import com.Rest.GolfMax.API.Services.Interfaces.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
-@RestController
+@Controller
 @RequestMapping("/users/auth")
 public class RegistrationController {
 
@@ -28,16 +30,33 @@ public class RegistrationController {
         this.USER_SERVICE = userService;
     }
 
-    @PostMapping("/signup")
-    public ResponseEntity<UserDto> registerUser(@RequestBody UserDto userRequest) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    @PostMapping("/signup/verification")
+    public ResponseEntity<UserDto> registerUser(@RequestBody UserDto userRequest, HttpServletRequest request)
+        throws UnsupportedEncodingException, MessagingException,
+            NoSuchAlgorithmException, InvalidKeySpecException {
         if (!USER_SERVICE.isValidRegistrationRequest(userRequest.getUsername(), userRequest.getEmail())) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
         else {
             User user = modelMapper.map(userRequest, User.class);
-            User registeredUser = USER_SERVICE.createUser(user);
+            User registeredUser = USER_SERVICE.registerUser(user, getSiteURL(request));
             UserDto userResponse = modelMapper.map(registeredUser, UserDto.class);
             return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
         }
+    }
+
+    @GetMapping("/verify")
+    public String verifyUser(@Param("verificationCode") String verificationCode) {
+        if (USER_SERVICE.isValidVerificationCode(verificationCode)) {
+            return "successful_verification";
+        }
+        else {
+            return "failed_verification";
+        }
+    }
+
+    private String getSiteURL(HttpServletRequest request) {
+        String siteURL = request.getRequestURL().toString();
+        return siteURL.replace(request.getServletPath(), "");
     }
 }
