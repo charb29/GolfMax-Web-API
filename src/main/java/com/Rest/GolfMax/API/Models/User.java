a9bb186423c9e60fec956b32df27ff508f69008b
charb29 <ocharb@mac.com>
2022-09-12 18:48:03 -0700

    Using Lombok for Getter and Setters

--- src/main/java/com/Rest/GolfMax/API/Models/User.java
+++ src/main/java/com/Rest/GolfMax/API/Models/User.java
@@ -1,11 +1,18 @@
 package com.Rest.GolfMax.API.Models;
 
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -40,76 +47,10 @@ public class User {
     @Column(name = "is_enabled")
     private boolean isEnabled;
 
-    public User() {}
-
     public User(Long id, String username, String password, String email) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-   public void setId(Long id) {
-        this.id = id;
-   }
-
-   public String getUsername() {
-        return username;
-   }
-
-   public void setUsername(String username) {
-        this.username = username;
-   }
-
-   public String getPassword() {
-        return password;
-   }
-
-   public void setPassword(String password) {
-        this.password = password;
-   }
-
-   public String getEmail() {
-        return email;
-   }
-
-   public void setEmail(String email) {
-        this.email = email;
-   }
-
-    public String getVerificationCode() {
-        return verificationCode;
-    }
-
-    public void setVerificationCode(String verificationCode) {
-        this.verificationCode = verificationCode;
-    }
-
-    public boolean isEnabled() {
-        return isEnabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        isEnabled = enabled;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 }
