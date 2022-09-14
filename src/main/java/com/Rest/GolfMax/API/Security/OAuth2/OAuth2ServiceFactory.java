538ea71e223cc7eb50c818bcefb0d1df40baf3e2
charb29 <ocharb@mac.com>
2022-09-13 19:02:05 -0700

    OAuth endpoints

--- src/main/java/com/Rest/GolfMax/API/Security/OAuth2/OAuth2ServiceFactory.java
+++ src/main/java/com/Rest/GolfMax/API/Security/OAuth2/OAuth2ServiceFactory.java
@@ -40,7 +40,6 @@ public class OAuth2ServiceFactory {
     }
 
     @RequiredArgsConstructor
-
     public class OAuth2Api extends DefaultApi20 {
 
         private final OAuth2Properties.Provider PROVIDER;
