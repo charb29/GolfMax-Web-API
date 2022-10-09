e57976c4af569a21944b5f3b0cbc9f5b7c619e5b
Olivier Charbonneau <ocharb@mac.com>
2022-10-08 23:23:44 -0700

    import spring framework web annotations

--- src/main/java/com/Rest/GolfMax/API/Controllers/LoginController.java
+++ src/main/java/com/Rest/GolfMax/API/Controllers/LoginController.java
@@ -5,7 +5,10 @@ import com.Rest.GolfMax.API.Services.Interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
